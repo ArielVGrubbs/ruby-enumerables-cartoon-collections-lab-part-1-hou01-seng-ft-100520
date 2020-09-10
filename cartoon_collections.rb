ef5a57820ee6198adfd900861204d5a1c74b4cb1
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-10 23:53:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |dwarf, index|
-    puts index
-    "#{index + 1}. #{dwarf}"
+    "#{index}. #{dwarf}"
   end
 end
